b30bb03ec286faca29d727452383c6bdd4d7eb05
Tomasz Gralak <tomaszgralak@Tomaszs-MacBook-Pro.local>
2023-02-22 14:18:25 +0100

    add graphql client

--- src/lib/create-graphq-client.ts
+++ src/lib/create-graphq-client.ts
@@ -1,4 +1,5 @@
 import { AuthConfig, authExchange } from "@urql/exchange-auth";
+import { initUrqlClient } from "next-urql";
 import {
   cacheExchange,
   createClient as urqlCreateClient,
@@ -9,6 +10,49 @@ import {
 interface IAuthState {
   token: string;
 }
+
+const getExchanges = (getAuth: AuthConfig<IAuthState>["getAuth"]) => [
+  dedupExchange,
+  cacheExchange,
+  authExchange<IAuthState>({
+    addAuthToOperation: ({ authState, operation }) => {
+      if (!authState || !authState?.token) {
+        return operation;
+      }
+
+      const fetchOptions =
+        typeof operation.context.fetchOptions === "function"
+          ? operation.context.fetchOptions()
+          : operation.context.fetchOptions || {};
+
+      return {
+        ...operation,
+        context: {
+          ...operation.context,
+          fetchOptions: {
+            ...fetchOptions,
+            headers: {
+              ...fetchOptions.headers,
+              "Authorization-Bearer": authState.token,
+            },
+          },
+        },
+      };
+    },
+    getAuth,
+  }),
+  fetchExchange,
+];
+
+export const nextClient = (url: string, getAuth: AuthConfig<IAuthState>["getAuth"]) => {
+  return initUrqlClient(
+    {
+      url,
+      exchanges: getExchanges(getAuth),
+    },
+    false
+  );
+};
 
 export const createClient = (url: string, getAuth: AuthConfig<IAuthState>["getAuth"]) =>
   urqlCreateClient({
