2199c3f4200fcf38578d977cf2e600544eee18ef
Tomasz Gralak <tomaszgralak@Tomaszs-MacBook-Pro.local>
2023-02-23 11:36:41 +0100

    test validation rules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
 import { NextPage } from "next";
-import { useFetchAllCheckoutsQuery } from "../../generated/graphql";
+import {
+  CountryCode,
+  useAddressValidationRulesQuery,
+  useCheckoutCreateMutation,
+  useFetchAllCheckoutsQuery,
+} from "../../generated/graphql";
 
 const ExamplePage: NextPage = () => {
   const [{ data, error }] = useFetchAllCheckoutsQuery();
+  const [{ data: addressValidationRulesData }] = useAddressValidationRulesQuery({
+    variables: {
+      countryCode: CountryCode.Pl,
+    },
+  });
+  const [createCheckoutData, createCheckout] = useCheckoutCreateMutation();
+
+  const handleCheckoutCreate = () => {
+    createCheckout({});
+    console.log(createCheckoutData);
+  };
+
+  console.log(addressValidationRulesData);
 
   if (!data?.checkouts) {
     return <div>Loading...</div>;
@@ -15,6 +33,7 @@ const ExamplePage: NextPage = () => {
   return (
     <div>
       <h1>Example page</h1>
+      <button onClick={handleCheckoutCreate}>test create checkout</button>
       <main>
         {data.checkouts.edges.map(({ node }) => (
           <p key={node.id}>{node.id}</p>
