e45d4a06e260b74f8881f358518a312f4e779523
Wojciech Gajda <wojciechgajda@Wojciechs-MacBook-Pro.local>
2023-02-23 16:02:15 +0100

    delete unused variables

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,12 +1,5 @@
-import {
-  ApolloClient,
-  ApolloLink,
-  HttpLink,
-  InMemoryCache,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 require("dotenv").config();
-
-export const ORDERS_APP_CLIENT = "ORDERS_APP_CLIENT";
 const API_URI = process.env.NEXT_PUBLIC_API_URI;
 
 export const client = new ApolloClient({
